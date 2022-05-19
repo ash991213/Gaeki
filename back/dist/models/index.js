85dffb0c80ddbf274206962262b295dfccba193d
ash991213 <ads1213@nate.com>
2022-05-19 09:13:18 +0900

    pages

--- back/dist/models/index.js
+++ back/dist/models/index.js
@@ -1,5 +1,5 @@
 'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
+Object.defineProperty(exports, "__esModule", { value: true });
 const fs = require('fs');
 const path = require('path');
 const { Sequelize, DataTypes } = require('sequelize');
@@ -9,28 +9,28 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 let sequelize;
 if (config.use_env_variable) {
-	sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-	sequelize = new Sequelize(config.database, config.username, config.password, config);
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+}
+else {
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 const User = require('./user.js')(sequelize, DataTypes);
 const Auto_Gold = require('./auto_Gold.js')(sequelize, DataTypes);
 const Auto_Exp = require('./auto_Exp.js')(sequelize, DataTypes);
 const Status = require('./status.js')(sequelize, DataTypes);
 const Counte = require('./counter.js')(sequelize, DataTypes);
-
 fs.readdirSync(__dirname)
-	.filter((file) => {
-		return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
-	})
-	.forEach((file) => {
-		const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-		db[model.name] = model;
-	});
+    .filter((file) => {
+    return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
+})
+    .forEach((file) => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+});
 Object.keys(db).forEach((modelName) => {
-	if (db[modelName].associate) {
-		db[modelName].associate(db);
-	}
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
 });
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
