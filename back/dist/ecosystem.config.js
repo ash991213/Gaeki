64571fd6e86fc221d7683dce7145dc332b2a743b
yoonk1228 <yoonk1228@naver.com>
2022-06-02 18:43:40 +0900

    배포 pm2 설정

---
+++ back/dist/ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps[
+        {
+            name: 'Gaeki-sequelize-pm2',
+            script: './server.js',
+            watch: false,
+            autorestart: true,
+        }
+    ]
+}
