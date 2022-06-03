4524e447580d14fe80cb8bb71755448b26003ee7
yoonk1228 <yoonk1228@naver.com>
2022-06-03 16:59:04 +0900

    fix : save File

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -15,21 +15,6 @@ const loadding: NextPage = () => {
 
     return (
         <Loadding>
-<<<<<<< HEAD
-            {/* <a className="wrap" href="http://localhost:4000/kakao/login"> */}
-            {/* <a className="wrap" href="http://15.164.141.28/kakao/login"> */}
-            {/* <a className="wrap" href="http://localhost:4000/api"> */}
-            <a className="wrap" href="http://15.164.141.28:81/kakao/login">
-                {/* http://15.164.141.28/ */}
-                <div className="header">
-                    <h1 className="logo">개발자 키우기 Logo~</h1>
-                </div>
-                <div className="loading">
-                    <img src="./loading.gif" />
-                </div>
-                <div className="loading_bar">화면을 터치해주세요</div>
-            </a>
-=======
             {loading === true ? (
                 <a className="wrap" href="http://localhost:4000/kakao/login">
                     <div className="header">
@@ -53,7 +38,6 @@ const loadding: NextPage = () => {
                     </div>
                 </span>
             )}
->>>>>>> b6aa464ca338aba2f1df588cfd6ba0e9c00a1b3a
         </Loadding>
     );
 };
